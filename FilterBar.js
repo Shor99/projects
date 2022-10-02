f0093a28c85f657b8935bfc8de9d9eee7fe07822
Shor99 <89186854+Shor99@users.noreply.github.com>
2022-10-02 18:03:32 +0300

    first commit

---
+++ FilterBar.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+function FilterBar(props) {
+    const [name,setName] = useState('');
+    const [status,setStatus] = useState('');
+    function handleBtnClick(e){
+        e.preventDefault();
+        props.addServices({
+            id: Math.random() + '',
+            name: name,
+            status: status,
+        });
+        console.log(name);
+        console.log(status);
+    }
+    return (
+        <>
+            <div className="d-flex p-4 border align-items-center">
+                <label className="me-2">Service Name:</label>
+                <select value={name} onChange={(e) => setName(e.target.value)} className="form-select w-25 me-2" aria-label="Default select example">
+                    <option >Mailling List</option>
+                    <option>WhatsApp Group Notification</option>
+                </select>
+                <label className="me-2">Status:</label>
+                <select value={status} onChange={(e) => setStatus(e.target.value)} className="form-select w-25 me-2" aria-label="Default select example">
+                    <option >active</option>
+                    <option>disabled</option>
+                    <option>banned</option>
+                </select>
+                <button onClick={(e) => handleBtnClick(e)} type="submit" className="btn btn-transparent border border-success text-success">Add Service</button>
+            </div>
+        </>
+        
+     );
+}
+
+export default FilterBar;
